029e102bc5bad7ff96a3a7748c3658647f6204ef
ysalfafara <ysalfafara@gmail.com>
2016-11-14 21:37:16 +0800

    Created a success page to inform the user that the database inputted the records. As well as added some code to the controller form file

--- Exercise6/codeigniter/application/controllers/Form.php
+++ Exercise6/codeigniter/application/controllers/Form.php
@@ -2,9 +2,17 @@
 
 class Form extends CI_Controller {
 
-	public function index(){
-		$this->load->view('Form.php'); 
-		$this->load->helper('html');
-	}
+        public function index()
+        {
+                $this->load->helper(array('form', 'url'));
 
+                $this->load->library('form_validation');
+
+                if ($this->form_validation->run() == FALSE){
+                        $this->load->view('Form.php');
+                }
+                else{
+                        $this->load->view('formsuccess');
+                }
+        }
 }
