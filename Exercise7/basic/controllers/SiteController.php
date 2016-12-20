ea7586904ea039143bcb2a645dadfcaf21fb6048
ysalfafara <ysalfafara@gmail.com>
2016-12-20 18:35:55 +0800

    Added the connection to the models

--- Exercise7/basic/controllers/SiteController.php
+++ Exercise7/basic/controllers/SiteController.php
@@ -8,6 +8,8 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\UserFormModel;
+use app\models\Trivias;
 
 class SiteController extends Controller
 {
