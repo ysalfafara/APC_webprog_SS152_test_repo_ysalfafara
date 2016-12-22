4fd8993e34b0bff21dcc4d2dba99b7a21b35f2b7
ysalfafara <ysalfafara@gmail.com>
2016-12-22 09:25:17 +0800

    Tested another file

---
+++ Exercise7/basic2/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=dbtuts',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
